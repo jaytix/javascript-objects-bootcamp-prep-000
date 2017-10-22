276b5f7e6c02807f3820eee1f0bc0a69fbaf1281
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-10-22 00:41:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {drake: "One Dance"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = 'songTitle';
+  playlist[artistName] = songTitle;
   return playlist;
 }
